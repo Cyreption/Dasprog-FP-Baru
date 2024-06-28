28573facbded421d833efd2156959585aca10594
Satria <162649169+Cyreption@users.noreply.github.com>
2024-06-28 15:29:23 +0700

    Add files via upload

---
+++ Final-Project-BP-IUP-ITS-master/src/Functions.java
@@ -0,0 +1,55 @@
+public final class Functions {
+    public static void printLoop(String input, int iterations) {
+        for (int i = 0; i < iterations; i++) {
+            System.out.print(input);
+        }
+    }
+
+    public static int getDigits10(int input) {
+        return (int) Math.log10(input) + 1;
+    }
+
+    // chiyoung part
+    public static int randomInt(int[] exc){
+        int res = (int) (Math.random()*6+1);
+        while(contains(exc,res) != -1){
+            res = (int) (Math.random()*6+1);
+        }
+        return res;
+    }
+
+    // chiyoung part
+    public static int contains(int[] array, int key) {
+        int count = 0;
+        for (int i : array) {
+            if (i == key) {
+                return count;
+            }
+            count++;
+        }
+        return -1;
+    }
+
+    // chiyoung part
+    public static int maxList(int[] L){
+        int n = L.length;
+        int max = L[0];
+        int index = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(L[i] > max) {
+                max = L[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static void wait(int amount) {
+        try {
+            Thread.sleep(amount);
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
---
+++ Final-Project-BP-IUP-ITS-master/src/Player.java
@@ -0,0 +1,73 @@
+public class Player {
+    private final int number;
+    private final String colour;
+    private String name = "Player ";
+    private int currentTile;
+    private int numberOfTurns = 0;
+    private int score = 0;
+    private boolean isStunned = false; // added missing field
+
+    public Player(int number, int currentTile, String colour){
+        this.number = number;
+        this.name += number;
+        this.currentTile = currentTile;
+        this.colour = colour;
+    }
+
+    public int getNumberOfTurns() {
+        return this.numberOfTurns;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getCurrentCase(){
+        return this.currentTile;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+
+    public boolean isStunned() {
+        return this.isStunned;
+    }
+
+    public void setStunned(boolean stunned) {
+        this.isStunned = stunned;
+    }
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setNumberOfTurns(int n) {
+        this.numberOfTurns = n;
+    }
+
+    public void addTurns(int n) {
+        this.numberOfTurns += n;
+    }
+
+    public void setScore(int n) {
+        this.score = n;
+    }
+
+    public void addScore(int n) {
+        this.score += n;
+    }
+
+    public void setCurrentCase(int n){
+        this.currentTile = n;
+    }
+
+    public void printStatus() {
+        System.out.println(this.getName() +  ":");
+
+        System.out.println("\tYou are on tile " + this.getCurrentCase());
+        System.out.println("\tYou have " + this.getScore() + " points");
+        System.out.println("\tYou have " + this.getNumberOfTurns() + " turns");
+    }
+}
